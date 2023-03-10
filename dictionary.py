83007575085148922c0d7c76a848ed474e9080ac
waithakan <waithakadylan@gmail.com>
2023-03-10 18:25:56 +0300

    assigmnets

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+# Write a program that uses a dictionary to store information about a person, such as their name, age, and favorite color. Ask the user for input and store the information in the dictionary. Then, print the dictionary to the console.
+
+
+# Create an empty dictionary to store the person's information
+person = {}
+
+# Ask the user for input and store the information in the dictionary
+person['name'] = input("Enter the person's name: ")
+person['age'] = input("Enter the person's age: ")
+person['favorite_color'] = input("Enter the person's favorite color: ")
+
+# Print the dictionary to the console
+print(person)
